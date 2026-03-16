00770331ef4db11c9622e70881adfeb67db9ba28
eungyeolbaek <eungyeolbaek@gmail.com>
2026-03-16 18:13:45 +0900

    refactor: common chips 리팩터링 및 폰트 적용

--- src/components/Chip/CategoryChip/CategoryChip.css.jsx
+++ src/components/Chip/CategoryChip/CategoryChip.css.jsx
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { vars } from '@/styles/token.css.js';
 
 export const categoryChip = style({
-  display: 'flex',
+  display: 'inline-flex',
   padding: '0.3125rem 0.4375rem',
   justifyContent: 'center',
   alignItems: 'center',
@@ -10,4 +10,11 @@ export const categoryChip = style({
 
   borderRadius: vars.radius.md,
   border: `1px solid ${vars.color.gray300}`,
+  background: `${vars.color.white}`,
+
+  color: `${vars.color.gray600}`,
+  fontSize: '0.8125rem',
+  fontStyle: 'normal',
+  fontWeight: '500',
+  lineHeight: 'normal',
 });
--- src/styles/globals.css.js
+++ src/styles/globals.css.js
@@ -13,3 +13,24 @@ globalStyle('a', {
 globalStyle('*', {
   boxSizing: 'border-box',
 });
+
+globalStyle('@font-face', {
+  fontFamily: 'Quantico',
+  src: "url('/fonts/Quantico-Bold.woff2') format('woff2')",
+  fontWeight: 700,
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+});
+
+globalStyle('@font-face', {
+  fontFamily: 'Pretendard',
+  src: "url('/fonts/Pretendard-Regular.woff2') format('woff2')",
+  fontWeight: 400,
+  fontStyle: 'normal',
+  fontDisplay: 'swap',
+});
+
+globalStyle('body', {
+  fontFamily: 'Pretendard, sans-serif',
+  fontWeight: 400,
+});
