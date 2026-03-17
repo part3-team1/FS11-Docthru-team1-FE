4a1a02d000d43ea6860777286a7969f33e10fa77
ZoeYoon <bus1990@naver.com>
2026-03-17 16:04:18 +0900

    style: 하단 padding수정, 배경색깔 gray50적용

--- src/domain/Home/Container/HomeContainer.css.js
+++ src/domain/Home/Container/HomeContainer.css.js
@@ -1,8 +1,9 @@
-import { media } from "@/styles/token.css";
+import { media, vars } from "@/styles/token.css";
 import { style } from "@vanilla-extract/css";
 
 export const container = style({
   paddingTop: '3.75rem',
+  backgroundColor:vars.color.gray50,
   '@media': {
     [media.mobile]: {
       paddingTop:'3.5rem'
