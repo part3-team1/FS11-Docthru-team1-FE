7e362a85647bc272ba627ee35733e03296c2b02e
JoohyoungJun <bmwjjh1130@gmail.com>
2026-03-17 19:09:35 +0900

    Refactor: 공용컴포넌트 CategoryDropdown 수정

--- src/app/(with-header)/(user)/layout.js
+++ src/app/(with-header)/(user)/layout.js
@@ -1,10 +1,8 @@
-import CategoryDropdown from '@/components/Dropdown/CategoryDropdown/CategoryDropdown';
 import UserHeader from '@/components/Layout/UserHeader/UserHeader.jsx';
 
 export default function WithUserHeaderLayout({ children }) {
   return (
     <>
-      <CategoryDropdown />
       <UserHeader />
       {children}
     </>
