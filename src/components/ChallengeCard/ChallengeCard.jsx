4d66bc7eab7d9638f670812b120b848a59a75648
eungyeolbaek <eungyeolbaek@gmail.com>
2026-03-19 04:08:36 +0900

    feat: 챌린지카드 범용성 확대 및 반응형 설정

--- src/components/ChallengeCard/ChallengeCard.jsx
+++ src/components/ChallengeCard/ChallengeCard.jsx
@@ -1,28 +1,45 @@
+import clsx from 'clsx';
+import Link from 'next/link';
 import ChallengeParticipantStatus from '../ChallengeParticipantStatus';
 import { CategoryChip, TypeChip } from '../Chip';
 import DueDate from '../DueDate';
 import * as styles from './ChallengeCard.css';
 
-export default function ChallengeCard({ challenge }) {
+export default function ChallengeCard({
+  challenge,
+  statusBadge,
+  topRight,
+  action,
+}) {
   return (
-    <div className={styles.challengeCard}>
-      <div className={styles.challengeTop}>
-        {challenge.title}
-        <div className={styles.chipWrapper}>
-          <TypeChip type={challenge.type} />
-          <CategoryChip category={challenge.category} />
+    <Link href={`/challenges/${challenge.id}`}>
+      <div
+        className={clsx(
+          styles.challengeCard,
+          statusBadge ? styles.heightWithStatus : styles.heightDefault,
+        )}
+      >
+        <div className={styles.challengeTop}>
+          {statusBadge}
+          {topRight && <div className={styles.topRight}>{topRight}</div>}
+          <h2 className={styles.title}>{challenge.title}</h2>
+          <div className={styles.chipWrapper}>
+            <TypeChip type={challenge.type} />
+            <CategoryChip category={challenge.category} />
+          </div>
         </div>
-      </div>
 
-      <div className={styles.challengeBottom}>
-        <div className={styles.infoWrapper}>
-          <DueDate dueDate={challenge.dueDate} />
-          <ChallengeParticipantStatus
-            current={challenge.current_participants}
-            max={challenge.max_participants}
-          />
-        </div>
+        <div className={styles.challengeBottom}>
+          <div className={styles.infoWrapper}>
+            <DueDate dueDate={challenge.dueDate} />
+            <ChallengeParticipantStatus
+              current={challenge.current_participants}
+              max={challenge.max_participants}
+            />
+          </div>
+          {action && <div className={styles.action}>{action}</div>}
       </div>
-    </div>
+        </div>
+    </Link>
   );
 }
