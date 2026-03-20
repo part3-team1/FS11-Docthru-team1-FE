a4e6dadcf4455cdeec98bd967fda36e2fa646ef9
ZoeYoon <bus1990@naver.com>
2026-03-20 14:35:57 +0900

    feat: new-Challenge(챌린지 신청폼) + 반응형 + 카테고리 드롭다운 수정

--- src/domain/NewChallenge/Components/NewChallengeForm.css.js
+++ src/domain/NewChallenge/Components/NewChallengeForm.css.js
@@ -4,7 +4,6 @@ import { style } from '@vanilla-extract/css';
 export const container = style({
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'center',
   gap: vars.space.xl,
 });
 
@@ -16,6 +15,19 @@ export const section = style({
   '@media': {
     [media.mobile]: {
       width: '21.4375rem',
+    },
+  },
+});
+
+export const title = style({
+  fontSize: '1.25rem',
+  fontWeight: 600,
+  display: 'flex',
+  alignSelf: 'flex-start',
+  '@media': {
+    [media.mobile]: {
+      fontSize: '1.125rem',
+      fontWeight: 700,
     },
   },
 });
@@ -41,4 +53,43 @@ export const input = style({
       border: '1px solid #24d62a',
     },
   },
+});
+
+export const textarea = style({
+  padding: '0.69rem 1.25rem',
+  height: '13.6875rem',
+  resize: 'none',
+  border: `1px solid ${vars.color.gray200}`,
+  borderRadius: vars.radius.xl,
+  outline: 'none',
+  '::placeholder': {
+    color: vars.color.gray400,
+  },
+  selectors: {
+    '&[aria-invalid="true"]': {
+      border: `1px solid ${vars.color.error}`,
+    },
+    '&[aria-invalid="false"]': {
+      border: '1px solid #24d62a',
+    },
+  },
+});
+
+export const message = style({
+  fontSize: '0.8rem',
+  color: vars.color.error,
+});
+
+export const btn = style({
+  padding: '0.88rem 7.88rem',
+  width: '36.875rem',
+  backgroundColor: vars.color.brandBlack,
+  color: vars.color.brandWhite,
+  borderRadius: vars.radius.xl,
+  fontWeight: 600,
+  '@media': {
+    [media.mobile]: {
+      width:'21.4375rem'
+    }
+  }
 });
--- src/domain/NewChallenge/Container/NewChallengeContainer.jsx
+++ src/domain/NewChallenge/Container/NewChallengeContainer.jsx
@@ -1,11 +1,10 @@
-import NewChallengeForm from "../Components/NewChallengeForm";
-import * as styles from './NewChallengeContainer.css'
+import NewChallengeForm from '../Components/NewChallengeForm';
+import * as styles from './NewChallengeContainer.css';
 
 export default function NewChallengeContainer() {
   return (
     <div className={styles.container}>
-      <div className={styles.title}>신규 챌린지 신청</div>
-      <NewChallengeForm/>
+      <NewChallengeForm />
     </div>
-  )
+  );
 }
