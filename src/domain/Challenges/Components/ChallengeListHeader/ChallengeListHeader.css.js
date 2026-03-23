df954b622308abbf170ada659e933dde1b475394
eungyeolbaek <eungyeolbaek@gmail.com>
2026-03-23 13:28:08 +0900

    feat: ChallengeListHeader 컴포넌트 제작

---
+++ src/domain/Challenges/Components/ChallengeListHeader/ChallengeListHeader.css.js
@@ -0,0 +1,21 @@
+import { media, vars } from '@/styles/token.css';
+import { style } from '@vanilla-extract/css';
+
+export const titleBar = style({
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+});
+
+export const title = style({
+  color: `${vars.color.gray800}`,
+  fontSize: '1.25rem',
+  fontStyle: 'normal',
+  fontWeight: '600',
+  lineHeight: 'normal',
+});
+
+export const filterBar = style({
+  display: 'flex',
+  gap: `${vars.radius.md}`
+});
