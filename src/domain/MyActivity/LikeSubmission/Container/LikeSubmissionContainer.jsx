1463ad139ace516e5f0408b7907d315aceee94f5
ZoeYoon <bus1990@naver.com>
2026-03-25 16:34:07 +0900

    목데이터 수정

--- src/domain/MyActivity/LikeSubmission/Container/LikeSubmissionContainer.jsx
+++ src/domain/MyActivity/LikeSubmission/Container/LikeSubmissionContainer.jsx
@@ -1,9 +1,14 @@
 import TableColumn from "@/components/TableColumn/TableColumn";
+import { likeSubmissionsData } from "@/mock/myActivityMockData";
 
 export default function LikeSubmissionContainer() {
+  const list = likeSubmissionsData.data.hearts.map((heart) => ({
+    id: heart.id,
+    title:heart.title
+  }))
   return (
     <div>
-      <TableColumn option='No.' data='순서' field='int' />
+      <TableColumn option='제목' data={list} field='submissionTitle' />
     </div>
   )
 }
