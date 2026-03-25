f183507e0fc9d5b8c85fecd68bca1947b4db448e
eungyeolbaek <eungyeolbaek@gmail.com>
2026-03-25 16:30:00 +0900

    refactor: 폴더구조 변경 및 라우터 연결

--- src/app/(with-header)/(user)/my-page/my-challenge/my-submissions/page.js
+++ src/app/(with-header)/(user)/my-page/my-challenge/my-submissions/page.js
@@ -1,10 +1,5 @@
-import MySubmissionContainer from '@/domain/MySubmission/Container/MySubmissionContainer';
+import MySubmissionContainer from '@/domain/MyChallenge/MySubmission/Container/MySubmissionContainer';
 
 export default function Page() {
-  return (
-    <>
-      작업물 보기
-    </>
-  );
-  // return <MySubmissionContainer />;
+  return <MySubmissionContainer />;
 }
