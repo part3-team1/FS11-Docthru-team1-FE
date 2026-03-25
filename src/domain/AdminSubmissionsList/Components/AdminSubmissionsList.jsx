c2a94fe56ddc5075ba3fcea76073a96f6669c8c3
Park Dodam <crayonpeenut@gmail.com>
2026-03-25 17:42:48 +0900

    Feat: AdminSubmissionsList ChallengeDetail UI 연결

--- src/domain/AdminSubmissionsList/Components/AdminSubmissionsList.jsx
+++ src/domain/AdminSubmissionsList/Components/AdminSubmissionsList.jsx
@@ -1,10 +1,35 @@
-'use client';
+import { ChallengeInfoAndButton } from '@/domain/ChallengeDetail/Components/ChallengeInfoAndButton/ChallengeInfoAndButton';
+import BestSubmissionCard from '@/domain/ChallengeDetail/Components/BestSubmissionCard/BestSubmissionCard';
+import ParticipationSubmissionList from '@/domain/ChallengeDetail/Components/ParticipationSubmissionList/ParticipationSubmissionList';
+import * as styles from '@/domain/ChallengeDetail/Container/ChallengeDetailContainer.css.js';
+import {
+  challengeDetailResponse,
+  currentUserMock,
+} from '@/mock/challengeDetailMockData.js';
 
 export default function AdminSubmissionsList({ id }) {
+  const data = challengeDetailResponse.data;
+  const currentUser = currentUserMock;
+  const bestItem = data?.participations?.items?.find(
+    (p) => p.submission.is_best === true,
+  );
+  const isClosed = data.status === 'CLOSED';
+
+  const isParticipating = currentUser
+    ? data?.participations?.items?.some((p) => p.user.id === currentUser.id)
+    : false;
+
   return (
-    <div>
-      <p>챌린지 ID: {id}</p>
-      {/* To Do: 작업물 + 피드백 UI 구현 */}
+    <div className={styles.container}>
+      <ChallengeInfoAndButton
+        data={data}
+        currentUser={currentUser}
+        isParticipating={isParticipating}
+      />
+
+      {isClosed && <BestSubmissionCard item={bestItem} />}
+
+      <ParticipationSubmissionList data={data.participations} />
     </div>
   );
 }
