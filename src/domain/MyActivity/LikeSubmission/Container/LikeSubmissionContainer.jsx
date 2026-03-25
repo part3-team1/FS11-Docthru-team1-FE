eb18da3733d07ae29bdd11354729c0c56907e4ab
ZoeYoon <bus1990@naver.com>
2026-03-25 16:11:36 +0900

    fix:폴더 및 파일위치 수정

---
+++ src/domain/MyActivity/LikeSubmission/Container/LikeSubmissionContainer.jsx
@@ -0,0 +1,9 @@
+import TableColumn from "@/components/TableColumn/TableColumn";
+
+export default function LikeSubmissionContainer() {
+  return (
+    <div>
+      <TableColumn option='No.' data='순서' field='int' />
+    </div>
+  )
+}
