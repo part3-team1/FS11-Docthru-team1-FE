880c8ccf4ef7efeefdc9ac82230738c8b575a530
Eungyeol Baek <eungyeolBaek@gmail.com>
2026-04-03 13:39:27 +0900

    feat: 마이페이지 내가 신청한 챌린지 내비게이션 탈출과 배경색 수정 및 챌린지신청(승인대기중) 수정 경로 추가

---
+++ src/app/(with-header)/(without-nav)/my-page/my-challenge/requested/[challengeRequestId]/edit/page.js
@@ -0,0 +1,8 @@
+//import ChallengeRequestEditContainer from '@/domain/뭐시기';
+
+export default function Page() {
+    // return <ChallengeRequestEditContainer />;
+}
+
+// 컨테이너 위치랑 이름 모르겠어서 대충 넣어봤음다
+// 주석 해제하고 컨테이너 넣어주십쇼
