4e01685fc2107c228f22bfcf76244663dbd0b699
ZoeYoon <bus1990@naver.com>
2026-04-03 16:10:00 +0900

    feat: challengeRequest edit 페이지 api 연결 및 수정가능

--- src/app/(with-header)/(without-nav)/my-page/my-challenge/requested/[challengeRequestId]/edit/page.js
+++ src/app/(with-header)/(without-nav)/my-page/my-challenge/requested/[challengeRequestId]/edit/page.js
@@ -1,8 +1,10 @@
-//import ChallengeRequestEditContainer from '@/domain/뭐시기';
+import ChallengeRequestEditContainer from '@/domain/NewChallenge/Container/ChallengeRequestEditContainer';
 
-export default function Page() {
-    // return <ChallengeRequestEditContainer />;
+export default async function Page({ params }) {
+  const { challengeRequestId } = await params;
+  return (
+    <>
+      <ChallengeRequestEditContainer id={challengeRequestId} />
+    </>
+  );
 }
-
-// 컨테이너 위치랑 이름 모르겠어서 대충 넣어봤음다
-// 주석 해제하고 컨테이너 넣어주십쇼
