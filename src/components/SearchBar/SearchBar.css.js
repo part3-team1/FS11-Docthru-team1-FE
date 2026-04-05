35aaf5103ba6ac2175a4d5f7ae011adb6f00aa22
JoohyoungJun <bmwjjh1130@gmail.com>
2026-04-06 01:38:55 +0900

    Design: 서치바, 필터드롭다운, 링크버튼 css 수정

--- src/components/SearchBar/SearchBar.css.js
+++ src/components/SearchBar/SearchBar.css.js
@@ -1,10 +1,9 @@
-import { vars } from '@/styles/token.css';
+import { media, vars } from '@/styles/token.css';
 import { style } from '@vanilla-extract/css';
 
 export const container = style({
   position: 'relative',
   width: '100%',
-
 });
 
 export const searchIcon = style({
@@ -13,18 +12,29 @@ export const searchIcon = style({
 });
 
 export const searchInput = style({
-    width: '100%',
+  width: '100%',
   height: '2.5rem',
   padding: '0.5rem 0.5rem 0.5rem 2.25rem',
 
   border: `1px solid ${vars.color.gray200}`,
   borderRadius: vars.radius.xxl,
-  '::placeholder': {
-    color: vars.color.gray400,
-    fontFamily: 'Pretendard',
-    fontSize: '1rem',
-    fontStyle: 'normal',
-    fontWeight: 400,
-    lineHeight: 'normal',
+
+  selectors: {
+    '&::placeholder': {
+      color: vars.color.gray400,
+      fontFamily: 'Pretendard',
+      fontSize: '1rem',
+      fontWeight: 400,
+    },
+  },
+
+  '@media': {
+    [media.mobile]: {
+      selectors: {
+        '&::placeholder': {
+          fontSize: '0.875rem',
+        },
+      },
+    },
   },
 });
